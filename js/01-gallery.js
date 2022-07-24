e0e3c7723531db550dafb8f264f9a0a574de16e3
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-07-24 17:27:22 +0200

    Zadanie 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const newGalleryItem = galleryItems.map((galleryItems) => `<div class="gallery__item">
+    <a class="gallery__link" href="${galleryItems.original}" >
+    <img
+        class="gallery__image"
+        src="${galleryItems.preview}"
+        data-source="${galleryItems.original}"
+        alt="${galleryItems.description}"/>
+    </a></div>`).join("");
+
+gallery.innerHTML = newGalleryItem;
+
+// DONE
