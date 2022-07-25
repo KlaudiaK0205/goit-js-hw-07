11773655d61f680758e437d53e7108b7d3f79a75
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2022-07-25 19:05:24 +0200

    Zadanie 1 i 2
    DONE???

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
-const newGalleryItem = galleryItems.map((galleryItems) => `<div class="gallery__item">
-    <a class="gallery__link" href="${galleryItems.original}" >
+const newGalleryItem = galleryItems.map((galleryItems) => `<div class='gallery__item'>
+    <a class='gallery__link' href='${galleryItems.original}' >
     <img
-        class="gallery__image"
-        src="${galleryItems.preview}"
-        data-source="${galleryItems.original}"
-        alt="${galleryItems.description}"/>
-    </a></div>`).join("");
+        class='gallery__image'
+        src='${galleryItems.preview}'
+        data-source='${galleryItems.original}'
+        alt='${galleryItems.description}'/>
+    </a></div>`).join(' ');
 
-gallery.insertAdjacentHTML("afterbegin", newGalleryItem);
+    gallery.innerHTML = newGalleryItem;
 
 // DONE
 
 // OPCJONALNE ZADANIE
 
-// import * as basicLightbox from 'basiclightbox'
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
 
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
+    const dataImg = event.target.getAttribute('data-source');
+    
+    const instance = basicLightbox.create(`<img src='${dataImg}' />`, {
+    
+    onShow: (instance) => {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') instance.close();
+        })
+    }
+});
 
-// instance.show()
+instance.show();
+
+});
+
+// DONE
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,19 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
-const newGalleryItem = galleryItems.map((galleryItem) => `<a class="gallery__item" href="${galleryItem.original}" >
-<img
-    class="gallery__image"
-    src="${galleryItem.preview}"
-    alt="${galleryItem.description}"
-    title="${galleryItem.description}"
-/>
-</a>`).join("");
+const newGalleryItem = galleryItems.map((galleryItem) => `<a class="gallery__item" href='${galleryItem.original}'>
+    <img 
+        class='gallery__image'
+        src='${galleryItem.preview}' 
+        alt='${galleryItem.description}' 
+        title = '${galleryItem.description}'/>
+    </a>`).join(' ');
 
-gallery.insertAdjacentHTML("afterbegin", newGalleryItem);
+gallery.innerHTML = newGalleryItem;
 
-new SimpleLightbox(".gallery a", { captionDelay: 250 });
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 // DONE
